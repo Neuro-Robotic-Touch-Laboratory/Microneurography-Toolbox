da515b949cef6bbeb334692185d8c5f0be1e800f
Giacomo D’Alesio <giacomo.dalesio@santannapisa.it>
2024-02-27 09:59:06 +0100

    files

---
+++ v1/MNG_toolbox_codes/sel_spikes.m
@@ -0,0 +1,33 @@
+function spike_res_out = sel_spikes(spike_res_in, ts, burst_ints)
+%SEL_SPIKES Summary of this function goes here
+%   Detailed explanation goes here
+
+
+spike_res_in.use_spikes = [];
+spike_res_out = spike_res_in;
+ts_msna  = ts(1) : ts(1) : ts(2);
+spike_res_out.use_spikes(1:length(spike_res_in.spike_idx),1) = ones(size(spike_res_in.spike_idx));
+for i = 1 : length(burst_ints)
+    spike_res_out.use_spikes(1:length(spike_res_in.spike_idx),i+1) = zeros(size(spike_res_in.spike_idx));
+    for j = 1:size(burst_ints(i).borders,1)
+        ts_idx =  find((spike_res_in.spike_ts/1000) >= burst_ints(i).borders(j,1) & (spike_res_in.spike_ts/1000) <= burst_ints(i).borders(j,2));
+        spike_res_out.use_spikes(ts_idx,i+1) = 1;
+    end
+end
+
+% for i =1 : length(burst_ints)
+%     spike_res_in.use_spikes(1:length(spike_res_in.spike_idx),i+1) = zeros(size(spike_res_in.spike_idx));
+%     for j = 1: size(burst_ints(i).borders)
+%         
+%         
+%         ts_idx = [find(ts_msna >= burst_ints(i).borders(j,1),1),...
+%                   find(ts_msna <= burst_ints(i).borders(j,2),1,'last')];
+%         tmp_idx(1) = find(spike_res_in.spike_idx >= ts_idx(1),1);
+%         tmp_idx(2) = find(spike_res_in.spike_idx <= ts_idx(2),1,'last');
+%         spike_res_in.use_spikes(tmp_idx(1):tmp_idx(2),i+1) = 1;
+%     end
+% end
+% 
+% spike_res_out = spike_res_in;
+end
+
---
+++ v1/MNG_toolbox_codes/update_signal_popup.m
@@ -0,0 +1,40 @@
+function update_signal_popup(app)
+%UPATE_SIGNAL_POPUP updates the selection options for all popup menues
+%for the selection of specific signals. to be called after calulation of
+%derived signals
+%   Detailed explanation goes here
+if ~isempty(app.data)
+    name = vertcat(app.data.name);
+    if length(app.data) < 2
+        i =1;
+    else
+        i = 2;
+    end
+else
+    name = {'none'};
+    i = 1;
+end
+app.popup_signal_spect.Items = name;
+app.popup_signal_spect.Value = name{1};
+
+app.popup_signal1.Items = name;
+app.popup_signal1.Value = name{1};
+
+app.popup_signal2.Items = name;
+app.popup_signal2.Value = name{i};
+
+% app.popup_entropy_signal.Items = name;
+% app.popup_entropy_signal.Value = name{1};
+
+app.popup_coorelation_signal1.Items = name;
+app.popup_coorelation_signal1.Value = name{1};
+
+app.popup_coorelation_signal2.Items = name;
+app.popup_coorelation_signal2.Value = name{i};
+
+app.popup_signal_to_predict.Items = name;
+app.popup_signal_to_predict.Value = name{1};
+app.popup_regressor.Items = name;
+app.popup_regressor.Value = name{1};
+end
+
